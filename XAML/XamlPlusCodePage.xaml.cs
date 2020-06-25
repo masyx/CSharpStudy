bf9e1ed42f559670226a06e501bc47876b2192e4
Sergey <rfmatveev@gmail.com>
2020-06-24 19:14:35 -0500

    Commit 06/24/2020

---
+++ XAML/XamlPlusCodePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XAML
+{
+    public partial class XamlPlusCodePage : ContentPage
+    {
+        public XamlPlusCodePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
